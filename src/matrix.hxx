a80059a22e330ad25b878d4d84aa65bd8dd95ef0
jwjbadger <jwjbadger@gmail.com>
2023-07-28 11:46:32 -0500

    +Matrix constructor
    adds ability to construct matrix with std::initializer_list

--- src/matrix.hxx
+++ src/matrix.hxx
@@ -1,10 +1,26 @@
 template <typename T>
-Matrix<T>::Matrix(std::initializer_list<std::vector<T>> data) : matrix(data) {
-	m = matrix.size();
-	n = matrix[0].size();
+Matrix<T>::Matrix(std::initializer_list<std::vector<T>> data) : _matrix(data) {
+	_size.m = _matrix.size();
+	_size.n = _matrix[0].size();
 }
+
+//template <typename T>
+//Matrix<T>::Matrix(std::initializer_list<std::vector<std::vector<T>>> data) : _matrix(data) {
+	//_size.m = _matrix.size();
+	//_size.n = _matrix[0].size();
+//}
 
 template <typename T>
 std::vector<T> Matrix<T>::operator[](const unsigned int r) {
-	return matrix[r];
+	return _matrix[r];
 }
+
+//template <typename T>
+//MSize Matrix<T>::size() {
+	//return _size;
+//}
+
+//template <typename T>
+//std::vector<std::vector<T>> Matrix<T>::data() {
+	//return _matrix;
+//}
--- src/vector.hxx
+++ src/vector.hxx
@@ -1,53 +1,109 @@
 template <typename T>
-Vector<T>::Vector(std::initializer_list<T> data) : vector(data) {
-	n = vector.size();
+Vector<T>::Vector(std::initializer_list<T> data) : _vector(data) {
+	_n = _vector.size();
 };
 
 template <typename T>
 T Vector<T>::operator[](const unsigned int r) {
-	return vector[r];
+	return _vector[r];
+}
+
+template <typename T>
+std::vector<T> Vector<T>::operator=(T data[]) {
+	_vector = data;
+	return _vector;
 }
 
 template <typename T>
 Vector<T>& Vector<T>::pop() {
-	vector.pop_back();
-	n = vector.size();
+	_vector.pop_back();
+	_n = _vector.size();
 	return *this;
 }
 
 template <typename T>
 Vector<T>& Vector<T>::push(T value) {
-	vector.push_back(value);
-	n = vector.size();
+	_vector.push_back(value);
+	_n = _vector.size();
 	return *this;
 }
 
 template <typename T>
-T Vector<T>::dot(Vector<T> b) {
-	T product = 0;
+Vector<T> Vector<T>::operator+(Vector<T> b) {
+	Vector<T> sum {};
 
-	for (int i = 0; i < n; i++) 
-		product += vector[i] * b[i];	
-	
-	return product;
+	for (int i = 0; i < _n; i++)
+		sum.push(_vector[i] + b[i]);
+
+	return sum;
+}
+
+template <typename T>
+Vector<T> Vector<T>::operator+(const T b) {
+	Vector<T> sum {};
+
+	for (int i = 0; i < _n; i++)
+		sum.push(_vector[i] + b);
+
+	return sum;
+}
+
+template <typename T>
+Vector<T> Vector<T>::operator-(Vector<T> b) {
+	Vector<T> diff {};
+
+	for (int i = 0; i < _n; i++)
+		diff.push(_vector[i] - b[i]);
+
+	return diff;
+}
+
+template <typename T>
+Vector<T> Vector<T>::operator-(const T b) {
+	Vector<T> diff {};
+
+	for (int i = 0; i < _n; i++)
+		diff.push(_vector[i] - b);
+
+	return diff;
 }
 
 template <typename T>
 Vector<T> Vector<T>::operator*(const T b) {
 	Vector<T> product {};
 
-	for (int i = 0; i < n; i++)
-		product.push(vector[i] * b);	
+	for (int i = 0; i < _n; i++)
+		product.push(_vector[i] * b);	
+
+	return product;
+}
+
+template <typename T>
+T Vector<T>::dot(Vector<T> b) {
+	T product = 0;
+
+	for (int i = 0; i < _n; i++) 
+		product += _vector[i] * b[i];	
+	
+	return product;
+}
+
+template <typename T>
+Vector<T> Vector<T>::operator/(const T b) {
+	Vector<T> product {};
+
+	for (int i = 0; i < _n; i++)
+		product.push(_vector[i] / b);	
 
 	return product;
 }
 
 template <typename T>
 unsigned int Vector<T>::size() {
-	return n;
+	return _n;
 }
 
 template <typename T>
 std::vector<T> Vector<T>::data() {
-	return vector;
+	return _vector;
 }
